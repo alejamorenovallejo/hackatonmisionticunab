60c3b109cb2e59b534ad407106a5965111c7295f
alejamorenovallejo <alejamorenovallejo@gmail.com>
2021-12-04 15:44:33 -0500

    Se crea el api de Punto de Venta

---
+++ Spring Boot Project/src/main/java/com/ejemplo/tiendaalamano/repository/PuntoVentaRepository.java
@@ -0,0 +1,12 @@
+package com.ejemplo.tiendaalamano.repository;
+
+import com.ejemplo.tiendaalamano.model.PuntoVenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Leandro Aguirre
+ */
+public interface PuntoVentaRepository extends JpaRepository<PuntoVenta, Long>{
+    
+}
---
+++ Spring Boot Project/src/main/java/com/ejemplo/tiendaalamano/service/PuntoVentaService.java
@@ -0,0 +1,12 @@
+package com.ejemplo.tiendaalamano.service;
+
+import com.ejemplo.tiendaalamano.model.PuntoVenta;
+import java.util.List;
+
+/**
+ *
+ * @author Leandro Aguirre
+ */
+public interface PuntoVentaService {
+    public List<PuntoVenta> findAll();
+}
