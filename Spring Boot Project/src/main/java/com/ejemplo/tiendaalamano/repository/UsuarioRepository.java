26b897dc6d57b88fd10b8710265e6da7b0296ac7
LeoAguirreJ <leandroaguirre9812@gmail.com>
2021-12-04 11:58:52 -0500

    Se crea api de PQR

--- Spring Boot Project/src/main/java/com/ejemplo/tiendaalamano/repository/UsuarioRepository.java
+++ Spring Boot Project/src/main/java/com/ejemplo/tiendaalamano/repository/UsuarioRepository.java
@@ -1,9 +1,6 @@
 package com.ejemplo.tiendaalamano.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ejemplo.tiendaalamano.model.Usuario;
