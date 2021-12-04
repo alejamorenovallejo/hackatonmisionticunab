76fe2ce463da45c53c22bcca34b40b7bac550f62
alejamorenovallejo <alejamorenovallejo@gmail.com>
2021-12-04 15:22:58 -0500

    Se crea el api de Puntos

---
+++ Spring Boot Project/src/main/java/com/ejemplo/tiendaalamano/controller/PuntosController.java
@@ -0,0 +1,30 @@
+package com.ejemplo.tiendaalamano.controller;
+
+import com.ejemplo.tiendaalamano.model.Puntos;
+import com.ejemplo.tiendaalamano.repository.PuntosRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Leandro Aguirre
+ */
+
+@RestController
+@RequestMapping("/api")
+public class PuntosController {
+    @Autowired
+    private PuntosRepository puntosRepository;
+    
+    @RequestMapping(value = "/puntos/all", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public List<Puntos> getAllModels() {
+        return puntosRepository.findAll();
+    }
+    
+}
